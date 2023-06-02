9b3e8e2aa9268566edd8e81b941ecc4173834d0d
Odei Alba <odeialba@gmail.com>
2023-06-02 18:04:31 +0200

    First release

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_sortvoting';
 $plugin->release = '1.0.0';
-$plugin->version = 2023052504;
+$plugin->version = 2023060200;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
