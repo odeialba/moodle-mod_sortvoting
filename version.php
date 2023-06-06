e6ac3220033b1cb41cf695a65f4f36606ecf959b
Odei Alba <odeialba@gmail.com>
2023-06-06 08:41:12 +0200

    Bump the version number

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_sortvoting';
 $plugin->release = '1.0.0';
-$plugin->version = 2023060200;
+$plugin->version = 2023060600;
 $plugin->requires = 2022041908;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [];
